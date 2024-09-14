1d2c15d9f47709c9deb8ee8c9de6099fb82cfe16
Mattis Kristensen <mattismoel@gmail.com>
2024-09-14 17:50:00 +0200

    initial commit

---
+++ addons/machinery/machinery.gd
@@ -0,0 +1,14 @@
+@tool
+extends EditorPlugin
+
+
+func _enter_tree() -> void:
+  # add_custom_type("StateMachine", "Node", preload("state_machine.gd"), preload("res://icon.svg"))
+  # add_custom_type("State", "Node", preload("state.gd"), preload("res://icon.svg"))
+  pass
+
+
+func _exit_tree() -> void:
+  # remove_custom_type("StateMachine")
+  # remove_custom_type("State")
+  pass
