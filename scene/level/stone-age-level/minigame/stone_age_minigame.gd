f57277d3a4bd79d94a6af7b0089dc3c2c3e6951c
Ingvar Hahn Kristensen <ingvar@hahnkristensen.dk>
2024-09-19 20:39:32 +0200

    gondola up and not down

--- scene/level/stone-age-level/minigame/stone_age_minigame.gd
+++ scene/level/stone-age-level/minigame/stone_age_minigame.gd
@@ -1,6 +1,6 @@
 extends Minigame
 
-@export var duration_secs: float = 10.0
+@export var force_per_hit: float = 100
 
 @export_category("References")
 @export var projectile: Node2D
@@ -11,7 +11,7 @@ extends Minigame
 @onready var force_label: Label = %ForceLabel
 @onready var countdown_timer: Timer = %CountdownTimer
 
-var _mammoth_angriness: float = 0
+var _mammoth_hits: float = 0
 
 func _ready() -> void:
     countdown_timer.timeout.connect(_on_timeout)
@@ -21,7 +21,8 @@ func _process(delta: float) -> void:
     countdown_label.text = format_time_left()
 
 func _on_timeout() -> void:
-    finished.emit(_mammoth_angriness)
+    var force = _mammoth_hits*force_per_hit
+    finished.emit(force)
 
 func format_time_left() -> String:
     var time_left := countdown_timer.time_left
@@ -36,9 +37,9 @@ func _on_target_hit() -> void:
     mammoth.direction*=-1
     mammoth.speed+=10
     
-    _mammoth_angriness += 1
+    _mammoth_hits += 1
     
-    if _mammoth_angriness == 1:
+    if _mammoth_hits == 1:
         force_label.text = "1 hit"
     else:
-        force_label.text = "%d hits" % (_mammoth_angriness)
+        force_label.text = "%d hits" % (_mammoth_hits)
--- scene/level/stone-age-level/stone_age_gondola.gd
+++ scene/level/stone-age-level/stone_age_gondola.gd
@@ -1,9 +1,24 @@
 extends Gondola
 
-@export var gravity: float
+var _acceleration: float = 0
+var _speed: float = 0
 
-func _process(delta: float) -> void:
-    pass
+func _physics_process(delta: float) -> void:
+    if _acceleration != 0:
+        _speed+=_acceleration*delta
+        var delta_pos=_speed*delta
+        
+        position+=delta_pos*Vector2.UP
+        reached_height+=delta_pos
+        
+        if reached_height>tower.tower_stat.height:
+            _on_ride_finish()
+    
+func begin_ride(acceleration: float) -> void:
+    _acceleration = acceleration
+    print("Gondola going up with acceleration %f" % _acceleration)
 
-func begin_ride(force: float) -> void:
-    print("Gondola going up...")
+func _on_ride_finish() -> void:
+    _acceleration = 0
+    _speed = 0
+    super()
