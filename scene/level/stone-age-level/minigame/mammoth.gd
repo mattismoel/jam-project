814aed2400247972143919b41409d0c43edb140c
Ingvar Hahn Kristensen <ingvar@hahnkristensen.dk>
2024-09-20 02:05:11 +0200

    added blueprint

--- scene/level/stone-age-level/minigame/mammoth.gd
+++ scene/level/stone-age-level/minigame/mammoth.gd
@@ -4,7 +4,7 @@ extends Node2D
 var direction: Vector2
 
 func _ready() -> void:
-    direction = Vector2.LEFT
+    direction = Vector2.RIGHT
 
 func _physics_process(delta: float) -> void:
     if !get_viewport_rect().has_point(global_position):
