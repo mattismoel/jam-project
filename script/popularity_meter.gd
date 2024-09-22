cab22538c6182cf233a96df33f6b8bfd7a019e02
Mattis Kristensen <mattismoel@gmail.com>
2024-09-22 17:03:10 +0200

    begin implementation of height meter

--- script/popularity_meter.gd
+++ script/popularity_meter.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 @export var _tower: Tower
 @export var _skull: Sprite2D
+@export var _progress_bar: ProgressBar
 
 @export var _line_color: Color = Color.WHITE:
     set(_v):
@@ -19,10 +20,11 @@ extends Node2D
     set(_v):
         _skull_offset = _v
         queue_redraw()
-        
+
 @export var _line_length: int = 5:
     set(_v):
         _line_length = _v
+        _progress_bar.custom_minimum_size.x = _v
         queue_redraw()
 
 @export var _marker_count: int = 10:
@@ -40,24 +42,24 @@ func _draw() -> void:
     _initial_position = Vector2i(_tower.global_position - _tower.position) + _line_offset - Vector2i(0,_tower.bottom_pos_y)
     draw_skull()
     draw_score_markers()
-    
+
 func draw_score_markers() -> void:
     var _pixel_alignment_offset = Vector2.ONE/2
-    
+
     for n in _marker_count+1:
         var score = float(n)/_marker_count
         var height = _tower.calculate_height_with_specific_popularity_score(score)
-        
+
         var start_draw_pos = _initial_position - Vector2i(0,height)
         var end_draw_pos = start_draw_pos + Vector2i(_line_length,0)
         draw_line(Vector2(start_draw_pos)+_pixel_alignment_offset-Vector2(0,1),Vector2(end_draw_pos)+_pixel_alignment_offset-Vector2(0,1),_line_color,1)
-        
+
     draw_line(Vector2(_initial_position), Vector2(_initial_position - Vector2i(0, _tower.calculate_height_with_specific_popularity_score(1)+1)), _line_color,1)
 
 func draw_skull() -> void:
     var _pixel_alignment_offset = Vector2.ONE/2
     var height = _tower.calculate_height_with_specific_popularity_score(_tower.minimum_score)
-    
+
     var skull_pos = _initial_position - Vector2i(3,height)
     var end_draw_pos = skull_pos + Vector2i(_line_length+3,0)
     draw_line(Vector2(skull_pos)+_pixel_alignment_offset-Vector2(0,1),Vector2(end_draw_pos)+_pixel_alignment_offset-Vector2(0,1),Color.RED,3)
